d6e951c36a920d43f94fa187192cfd7b01aab77c
lexion13 <lexion13@gmail.com>
2017-12-26 14:22:03 +0900

    mod just little bit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template,request,redirect,url_for # For flask im
 from pymongo import MongoClient # Database connector
 from bson.objectid import ObjectId # For ObjectId to work
 import feedparser
+import os
 
 app = Flask(__name__)
 
@@ -21,5 +22,7 @@ def contact():
     content = request.values.get('content')
     return redirect('/')
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+if __name__ == "__main__":
+#    app.run(debug=True, host="localhost", port=8080)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, use_reloader=True, port=port)
