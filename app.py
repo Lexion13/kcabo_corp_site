72f3621063422e2d6998addcc06765fe2728557f
lexion13 <lexion13@gmail.com>
2017-12-26 12:43:33 +0900

    first

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template,request,redirect,url_for # For flask implementation
+from pymongo import MongoClient # Database connector
+from bson.objectid import ObjectId # For ObjectId to work
+import feedparser
+
+app = Flask(__name__)
+
+'''
+client = MongoClient('localhost', 27017)    #Configure the connection to the database
+db = client.kcabo    #Select the database
+coll = db.coll #Select the collection
+'''
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/action/contact', methods=['POST'])
+def contact():
+    email = request.values.get('email')
+    content = request.values.get('content')
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
