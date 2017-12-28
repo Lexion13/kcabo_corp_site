d57b71b7c25a2d9275e0c99db6951151ba0b9ae3
lexion13 <lexion13@gmail.com>
2017-12-28 16:59:49 +0900

    remove sendgrid

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ client = MongoClient('localhost', 27017)    #Configure the connection to the dat
 db = client.kcabo    #Select the database
 coll = db.coll #Select the collection
 '''
-sendgrid_username = "app84049026@heroku.com"
-sendgrid_password = "1211333s"
-
-app.config['SENDGRID_API_KEY'] = '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'
-app.config['SENDGRID_DEFAULT_FROM'] = 'info@kcabo.co.jp'
-mail = SendGrid(app)
 
 
 
@@ -32,12 +26,6 @@ def index():
 def contact():
     email = request.values.get('email')
     content = request.values.get('content')
-    mail.send_email(
-        from_email = 'info@@kcabo.co.jp',
-        to_email = 'info@kcab.co.jp',
-        subject = 'Subject',
-        text = email+content
-    )
 
     return redirect('/')
 
